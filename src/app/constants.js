533420e21bef57a1f3b5c2c36f9da6565a28399f
brettmt10 <brettmt10@gmail.com>
2026-01-28 16:19:52 -0500

    added red line visual

---
+++ src/app/constants.js
@@ -0,0 +1,11 @@
+export const red_line_stations = [
+  {name: 'Davis', coords: [42.39674, -71.121815]},
+  {name: 'Porter', coords: [42.3884, -71.119149]},
+  {name: 'Harvard', coords: [42.373362, -71.118956]},
+  {name: 'Central', coords: [42.365486, -71.103802]},
+  {name: 'Kendall/MIT', coords: [42.362491, -71.086176]},
+  {name: 'Charles/MGH', coords: [42.361166, -71.070628]},
+  {name: 'Park Street', coords: [42.356395, -71.062424]},
+  {name: 'Downtown Crossing', coords: [42.355518, -71.060225]},
+  {name: 'South Station', coords: [42.352271, -71.055242]}
+];
--- src/app/map.js
+++ src/app/map.js
@@ -1,3 +1,5 @@
+import { red_line_stations } from './constants.js';
+
 var corner1 = L.latLng(42.325532, -71.158761),
 corner2 = L.latLng(42.400203, -71.010778),
 bounds = L.latLngBounds(corner1, corner2);
@@ -12,3 +14,27 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
     maxZoom: 19,
     attribution: '© OpenStreetMap, © CartoDB'
 }).addTo(map);
+
+
+for (let i = 0; i < red_line_stations.length - 1; i++) {
+    var station = red_line_stations[i];
+    L.polyline([station.coords, red_line_stations[i + 1].coords], {
+        color: '#DA291C',
+        weight: 10,
+        opacity: 0.8,
+        lineJoin: 'round',
+        lineCap: 'round'
+    }).addTo(map);
+}
+
+for (let i = 0; i < red_line_stations.length; i++){
+    var station = red_line_stations[i];
+    L.circleMarker(station.coords, {
+        radius: 8,
+        fillColor: '#0e0d0d',
+        color: '#FFFFFF',
+        weight: 3,
+        opacity: 1,
+        fillOpacity: 1
+    }).addTo(map).bindPopup(station.name);
+}
