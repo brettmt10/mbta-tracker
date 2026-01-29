a288d9518634443037a7558740bcf3e4337043a3
brettmt10 <brettmt10@gmail.com>
2026-01-28 20:15:54 -0500

    add constants for real time updates and red line

---
+++ src/api/constants.py
@@ -0,0 +1,13 @@
+predictions_api_url = 'https://api-v3.mbta.com/predictions'
+
+station_ids = [
+    'place-sstat',
+    'place-dwnxg',
+    'place-pktrm',
+    'place-chmnl',
+    'place-knncl',
+    'place-cntsq',
+    'place-harsq',
+    'place-portr',
+    'place-davis'
+]
