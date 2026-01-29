574717fcbf968698d58d28064be1a7b1323fcc68
brettmt10 <brettmt10@gmail.com>
2026-01-29 16:14:52 -0500

    add helpful notes to constants

--- src/api/constants.py
+++ src/api/constants.py
@@ -1,5 +1,6 @@
 predictions_api_url = 'https://api-v3.mbta.com/predictions'
 
+# I got the station ids and destination ids from https://api-v3.mbta.com/stops with filter[route_type] = 1
 station_ids = [
     'place-sstat',
     'place-dwnxg',
@@ -12,6 +13,7 @@ station_ids = [
     'place-davis'
 ]
 
+# using these statically helps with api rate limits
 destination_ids = {
     "70067": "Ashmont/Braintree",
     "70068": "Alewife"
