55c9665ff1a515c9b5d58e6639cc708a967c6d90
brettmt10 <brettmt10@gmail.com>
2026-01-29 20:38:09 -0500

    function for util

--- src/api/utils/get_station_metadata.py
+++ src/api/utils/get_station_metadata.py
@@ -1,37 +1,22 @@
 import requests
 
-station_ids = [
-    'place-sstat',
-    'place-dwnxg',
-    'place-pktrm',
-    'place-chmnl',
-    'place-knncl',
-    'place-cntsq',
-    'place-harsq',
-    'place-portr',
-    'place-davis'
-]
-
-params = {
-    'filter[route_type]': 1
-}
-
-response = requests.get('https://api-v3.mbta.com/stops', params=params)
-res_json = response.json()
-
-data = res_json['data']
-print(data)
-exit()
-
-station_metadata = {}
-for station_id in station_ids:
-    station_metadata[station_id] = {
-        'stop_ids': []
+def get_station_metadata(station_ids):
+    params = {
+        'filter[route_type]': 1
     }
+    
+    response = requests.get('https://api-v3.mbta.com/stops', params=params)
+    data = response.json()['data']
 
-    for station_data in data:
-        if station_data['relationships']['parent_station']['data']['id'] == station_id:
-            station_metadata[station_id]['stop_ids'].append(station_data['id'])
-
-print(station_metadata)
-
+    station_metadata = {}
+    for station_id in station_ids:
+        station_metadata[station_id] = {
+            'stop_ids': []
+        }
+        
+        for station_data in data:
+            parent_id = station_data['relationships']['parent_station']['data']['id']
+            if parent_id == station_id:
+                station_metadata[station_id]['stop_ids'].append(station_data['id'])
+    
+    return station_metadata
