ea4d33564611efd64f34be3d7e08ba8f139dd6bb
brettmt10 <brettmt10@gmail.com>
2026-02-02 16:02:09 -0500

    auto refresh station times

---
+++ src/app/refresh.js
@@ -0,0 +1,16 @@
+async function getData(station_id) {
+    const url =`http://localhost:8000/times/${station_id}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+setInterval(() => getData('place-harsq'), 5000);
