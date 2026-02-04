537d2174947374ef9b769771616580578f8480fb
brettmt10 <brettmt10@gmail.com>
2026-02-03 21:25:37 -0500

    additional reshape of station data

--- src/api/utils/get_station_metadata.py
+++ src/api/utils/get_station_metadata.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_station_metadata():
+def get_station_metadata_by_id():
     line_lookup = {
         "Alewife": 'Red',
         'Ashmont/Braintree': 'Red'
@@ -29,3 +29,34 @@ def get_station_metadata():
             station_metadata[id] = {"name": name, "direction": direction, "line": line, 'parent_station_id': parent_station}
     
     return station_metadata
+
+def get_station_metadata_by_parent():
+    line_lookup = {
+        "Alewife": 'Red',
+        'Ashmont/Braintree': 'Red'
+    }
+
+    params = {
+        'filter[route_type]': 1,
+        'fields[stop]': 'name,platform_name,latitude,longitude'
+    }
+
+    response = requests.get('https://api-v3.mbta.com/stops', params=params)
+    data = response.json()['data']
+
+    station_metadata = {}
+    for station in data:
+        directions = line_lookup.keys()
+        id = station['id']
+        direction = station['attributes']['platform_name']
+        coords = {'latitude': station['attributes']['latitude'], 'longitude': station['attributes']['longitude']}
+        name = station['attributes']['name']
+        parent_station = station['relationships']['parent_station']['data']['id']
+        if direction in directions:
+            line = line_lookup[direction]
+            station_metadata[parent_station] = {"name": name, "line": line, "coords": coords}
+            print(parent_station, name, line, coords)
+    
+    return station_metadata
+
+get_station_metadata_by_parent()
