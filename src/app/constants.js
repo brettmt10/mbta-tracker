e8e012c26f621ae9247b7e0a2ed77a9df2b91d86
brettmt10 <brettmt10@gmail.com>
2026-02-04 17:20:01 -0500

    filter refresh

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,18 +1,10 @@
-export const STATIONS = {
-  "place-qnctr": {
-    "name": "Quincy Center",
+export const STATIONS_RED = {
+  "place-sstat": {
+    "name": "South Station",
     "line": "Red",
     "coords": {
-      "latitude": 42.251809,
-      "longitude": -71.005409
-    }
-  },
-  "place-fldcr": {
-    "name": "Fields Corner",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.299935,
-      "longitude": -71.061945
+      "latitude": 42.352271,
+      "longitude": -71.055242
     }
   },
   "place-dwnxg": {
@@ -23,108 +15,12 @@ export const STATIONS = {
       "longitude": -71.060225
     }
   },
-  "place-cntsq": {
-    "name": "Central",
+  "place-pktrm": {
+    "name": "Park Street",
     "line": "Red",
     "coords": {
-      "latitude": 42.365379,
-      "longitude": -71.103554
-    }
-  },
-  "place-nqncy": {
-    "name": "North Quincy",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.275802,
-      "longitude": -71.030144
-    }
-  },
-  "place-asmnl": {
-    "name": "Ashmont",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.28453,
-      "longitude": -71.063725
-    }
-  },
-  "place-sstat": {
-    "name": "South Station",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.352271,
-      "longitude": -71.055242
-    }
-  },
-  "place-knncl": {
-    "name": "Kendall/MIT",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.362355,
-      "longitude": -71.085605
-    }
-  },
-  "place-portr": {
-    "name": "Porter",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.3884,
-      "longitude": -71.119149
-    }
-  },
-  "place-qamnl": {
-    "name": "Quincy Adams",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.233391,
-      "longitude": -71.007153
-    }
-  },
-  "place-brdwy": {
-    "name": "Broadway",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.342622,
-      "longitude": -71.056967
-    }
-  },
-  "place-harsq": {
-    "name": "Harvard",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.374663,
-      "longitude": -71.118814
-    }
-  },
-  "place-shmnl": {
-    "name": "Savin Hill",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.310591,
-      "longitude": -71.053612
-    }
-  },
-  "place-davis": {
-    "name": "Davis",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.39674,
-      "longitude": -71.121815
-    }
-  },
-  "place-wlsta": {
-    "name": "Wollaston",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.266788,
-      "longitude": -71.020457
-    }
-  },
-  "place-smmnl": {
-    "name": "Shawmut",
-    "line": "Red",
-    "coords": {
-      "latitude": 42.293126,
-      "longitude": -71.065738
+      "latitude": 42.356395,
+      "longitude": -71.062424
     }
   },
   "place-chmnl": {
@@ -135,20 +31,44 @@ export const STATIONS = {
       "longitude": -71.071505
     }
   },
-  "place-andrw": {
-    "name": "Andrew",
+  "place-knncl": {
+    "name": "Kendall/MIT",
     "line": "Red",
     "coords": {
-      "latitude": 42.330154,
-      "longitude": -71.057655
+      "latitude": 42.362355,
+      "longitude": -71.085605
     }
   },
-  "place-pktrm": {
-    "name": "Park Street",
+  "place-cntsq": {
+    "name": "Central",
     "line": "Red",
     "coords": {
-      "latitude": 42.356395,
-      "longitude": -71.062424
+      "latitude": 42.365379,
+      "longitude": -71.103554
+    }
+  },
+  "place-harsq": {
+    "name": "Harvard",
+    "line": "Red",
+    "coords": {
+      "latitude": 42.374663,
+      "longitude": -71.118814
+    }
+  },
+  "place-portr": {
+    "name": "Porter",
+    "line": "Red",
+    "coords": {
+      "latitude": 42.3884,
+      "longitude": -71.119149
+    }
+  },
+  "place-davis": {
+    "name": "Davis",
+    "line": "Red",
+    "coords": {
+      "latitude": 42.39674,
+      "longitude": -71.121815
     }
   }
 };
--- src/app/metadata_refresh.js
+++ src/app/metadata_refresh.js
@@ -3,8 +3,27 @@ const fs = require('fs');
 async function refreshStationMetadata() {
   const response = await fetch('http://localhost:8000/stations');
   const data = await response.json();
+
+  const supportedStationsRed = [
+    'place-sstat',
+    'place-dwnxg',
+    'place-pktrm',
+    'place-chmnl',
+    'place-knncl',
+    'place-cntsq',
+    'place-harsq',
+    'place-portr',
+    'place-davis'
+  ];
   
-  const fileContent = `export const STATIONS = ${JSON.stringify(data, null, 2)};\n`;
+  let filtered = {};
+  for (let parent_station of supportedStationsRed) {
+    if (data[parent_station]){
+      filtered[parent_station] = data[parent_station];
+    }
+  }
+
+  const fileContent = `export const STATIONS_RED = ${JSON.stringify(filtered, null, 2)};\n`;
   fs.writeFileSync('constants.js', fileContent);
 }
 
