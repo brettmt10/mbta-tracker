e1a0b6fdebee9d370c0c99c57cf6817d94799834
brettmt10 <brettmt10@gmail.com>
2026-02-11 20:31:37 -0500

    correct route filtering

--- src/api/mbta-api.py
+++ src/api/mbta-api.py
@@ -28,7 +28,7 @@ async def get_station_times(parent_station_id: str):
     params = {
         'filter[stop]': parent_station_id,
         'sort': 'time',
-        'filter[route_type]': '1'
+        'filter[route_type]': '0,1'
     }
 
     headers = {
@@ -39,6 +39,8 @@ async def get_station_times(parent_station_id: str):
     async with httpx.AsyncClient() as client:
         response = await client.get('https://api-v3.mbta.com/predictions', params=params, headers=headers)
         data = response.json()
+        print(parent_station_id, "--------------------\n------------\n--------------------\n------------\n\n----------------------\n------------", data)
+
         times = await parse_predicition_data(prediction_data=data, parent_station=parent_station_id, headers=headers)
 
     return {"res": times}
