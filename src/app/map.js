15b84a4f70156051dd401ed2d496bd64e6b31e20
brettmt10 <brettmt10@gmail.com>
2026-02-16 20:37:07 -0500

    remove old versions

--- src/app/map.js
+++ src/app/map.js
@@ -1,6 +1,4 @@
-import { STATIONS_RED } from './constants/stations.js';
-
-const parentStationsRed = Object.keys(STATIONS_RED);
+import { StationManager } from './manager.js';
 
 var corner1 = L.latLng(42.325532, -71.158761),
 corner2 = L.latLng(42.400203, -71.010778),
@@ -17,42 +15,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
     attribution: '© OpenStreetMap, © CartoDB'
 }).addTo(map);
 
-for (let i = 0; i < parentStationsRed.length - 1; i++) {
-    const station = STATIONS_RED[parentStationsRed[i]];
-    const nextStation = STATIONS_RED[parentStationsRed[i + 1]];
-    
-    const stationCoords =  L.latLng(station.coords.latitude, station.coords.longitude);
-    const nextStationCoords =  L.latLng(nextStation.coords.latitude, nextStation.coords.longitude);
-
-    L.polyline([stationCoords, nextStationCoords], {
-        color: '#DA291C',
-        weight: 10,
-        opacity: 0.8,
-        lineJoin: 'round',
-        lineCap: 'round'
-    }).addTo(map); 
+async function main() {
 }
 
-export const station_nodes = {}
-
-for (let i = 0; i < parentStationsRed.length; i++) {
-    const station = STATIONS_RED[parentStationsRed[i]];
-    
-    const stationCoords =  L.latLng(station.coords.latitude, station.coords.longitude);
-
-    var marker = L.circleMarker(stationCoords, {
-        radius: 8,
-        fillColor: '#0e0d0d',
-        color: '#FFFFFF',
-        weight: 3,
-        opacity: 1,
-        fillOpacity: 1
-    }).addTo(map);
-
-    
-    station_nodes[parentStationsRed[i]] = {
-        "marker": marker,
-        "data": null
-    };
-    
-}
+main();
