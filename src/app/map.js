ebf55cc2545e0442127416ca178029b0eef82b82
brettmt10 <brettmt10@gmail.com>
2026-02-17 16:12:45 -0500

    create a MapInstance class

--- src/app/map.js
+++ src/app/map.js
@@ -1,21 +1,8 @@
 import { StationManager } from './manager.js';
 
-var corner1 = L.latLng(42.325532, -71.158761),
-corner2 = L.latLng(42.400203, -71.010778),
-bounds = L.latLngBounds(corner1, corner2);
-
-const map = L.map('map').setView([42.356428, -71.078908], 14.5);
-map.setMaxBounds(bounds);
-map.options.maxBoundsViscosity = 1.0;
-map.setMinZoom(14);
-map.setMaxZoom(19);
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap, © CartoDB'
-}).addTo(map);
-
-async function main() {
+export class MapInstance {
+    constructor() {
+        this.manager = new StationManager();
+        this.map = L.map('map').setView([42.356428, -71.078908], 14.5);
+    }
 }
-
-main();
